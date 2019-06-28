aaf98be90620c505660019c0d24ef488d6aa040c
rodristu989 <rodristu989@gmail.com>
2019-06-28 18:19:40 -0400

    Final

---
+++ ProductBE/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductBE.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ProductBE.Models.Product> Products { get; set; }
+    }
+}
---
+++ ProductBE/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductBE.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        [Display(Name="Product name")]
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
